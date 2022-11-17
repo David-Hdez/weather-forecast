63df8444eb362c1b44c8bfd1d859be397cab9ee9
David Hernández <54561496+David-Hdez@users.noreply.github.com>
2022-11-17 11:38:30 -0600

    City selector
    Implement react-select.
    Consume API to get cities,
    autocomplete search name.

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,6 +1,51 @@
+import axios from "axios";
+
+import AsyncSelect from 'react-select/async';
+
 function Search() {
+    /**
+     * Cities autocomplete
+     * @param {string} inputValue 
+     * @param {*} callback 
+     * @see {@link https://react-select.com/async#loading-asynchronously}
+     */
+    const loadOptions = (
+        inputValue,
+        callback
+    ) => {
+        let options = {
+            method: 'GET',
+            url: 'https://spott.p.rapidapi.com/places/autocomplete',
+            params: { type: 'CITY', country: 'MX', limit: '5', 'language': 'es' },
+            headers: {
+                'X-RapidAPI-Key': import.meta.env.VITE_X_RAPIDAPI_KEY,
+                'X-RapidAPI-Host': 'spott.p.rapidapi.com'
+            }
+        };
+
+        if (inputValue) {
+            options.params.q = inputValue
+        };
+
+        axios.request(options).then(function (response) {
+            callback(response.data.map((city) => {
+                let city_name;
+
+                if (city.localizedName === null) {
+                    city_name = `${city.name}, ${city.adminDivision1.localizedName}`;
+                } else {
+                    city_name = `${city.localizedName}, ${city.adminDivision1.localizedName}`;
+                }
+
+                return { value: city.id, label: city_name }
+            }));
+        })
+    };
+
     return (
-        <input type="search" name="city" placeholder="Ciudad" />
+        <>
+            <AsyncSelect cacheOptions defaultOptions loadOptions={loadOptions} />
+        </>
     );
 }
 
