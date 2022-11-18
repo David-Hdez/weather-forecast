02c1fefe85d8e05760f3c17648e08db86a2702c4
David Hernández <54561496+David-Hdez@users.noreply.github.com>
2022-11-18 16:30:16 -0600

    Coordinates city selected
    Handle city detail selected.
    Props of component search
    will set the coordinates.

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from 'react';
 import reactLogo from './assets/react.svg'
 // import './App.css'
 import Container from 'react-bootstrap/Container';
@@ -6,18 +7,27 @@ import Menu from './components/Menu'
 import Search from './components/Search'
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Navbar from 'react-bootstrap/Navbar';
 
 function App() {
+  const [coordinates, setCoordinates] = useState(null);
+
+  const handleCity = (event) => {
+    setCoordinates(event);
+  }
+
   return (
     <Container>
       <Row>
         <Col>
-          <Menu></Menu>
+          <Navbar expand="lg" variant="light" bg="light">
+            <Navbar.Brand href="#">Pronóstico del tiempo</Navbar.Brand>
+          </Navbar>
         </Col>
       </Row>
       <Row>
         <Col>
-          <Search></Search>
+          <Search latLon={handleCity}></Search>
         </Col>
       </Row>
       <Row>
