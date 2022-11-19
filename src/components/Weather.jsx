d28966cddd16b2cfa83bb4e9a4ec3100a31e5328
David Hernández <54561496+David-Hdez@users.noreply.github.com>
2022-11-18 19:23:46 -0600

    Consume api forecast
    Weather component will receive
    props with coordinates.
    Will consume to get the
    forecast.

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,10 +1,41 @@
+import React, { useState, useEffect } from 'react';
 import Card from 'react-bootstrap/Card';
+import axios from "axios";
 
-function Weather() {
+function Weather(props) {
+    const [forecast, setForecast] = useState(null);
+
+    useEffect(
+        () => {
+            if (props.localizacion !== null) {
+                const options = {
+                    method: 'GET',
+                    url: 'https://api.openweathermap.org/data/2.5/weather',
+                    params: {
+                        appid: import.meta.env.VITE_APPID_OPENWEATHERMAP,
+                        lat: props.localizacion.latitude,
+                        lon: props.localizacion.longitude,
+                        units: 'metric'
+                    }
+                };
+
+                axios.request(options).then(function (response) {
+                    setForecast(response.data);
+                }).catch(function (error) {
+                    console.error(error);
+                });
+            }
+        },
+        [props.localizacion],
+    );
+
     return (
-        <Card>
-            <Card.Body>This is some text within a card body.</Card.Body>
-        </Card>
+        <>
+            {forecast
+                ? <Card><Card.Body>Seleccionada</Card.Body></Card>
+                : <Card><Card.Body>Selecciona una ciudad</Card.Body></Card>
+            }
+        </>
     );
 }
 
